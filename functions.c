2a765a3a8dab0eaa3292528cc7b1ef6b217bd47a
Sowjan95 <54956361+Sowjan95@users.noreply.github.com>
2019-10-29 16:01:10 -0400

    Update functions.c

--- functions.c
+++ functions.c
@@ -85,26 +85,3 @@ struct node * remove_node(struct node *front, int data){
   }
   return first;
 }
-
-
-int main(){
-  printf("Printing random list:\n");
-  struct node *list = create_list();
-  print_list(list);
-  printf("Inserting 5 in front:\n");
-  list = insert_front(list, 5);
-  print_list(list);
-  printf("Removing 3:\n");
-  list = remove_node(list, 3);
-  print_list(list);
-  printf("Removing 4:\n");
-  list = remove_node(list, 4);
-  print_list(list);
-  printf("Removing 7:\n");
-  list = remove_node(list, 7);
-  print_list(list);
-  printf("Freeing list.\n");
-  list = free_list(list);
-  printf("Printing freed list:\n");
-  print_list(list);
-}
